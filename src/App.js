a94de62b310b6f41c0847aed7e9530b99e748202
savio47 <37936071+savio47@users.noreply.github.com>
2021-03-19 20:24:20 +0530

    Add files via upload
    19 March Review (Login and HomePage)

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import fire from './config/fire';
+import Login from './Login'
+import Home from './Home'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      user : {}
+    }
+  }
+  componentDidMount()
+  {
+    this.authListener();
+  }
+  authListener(){
+    fire.auth().onAuthStateChanged((user)=>{
+      if(user)
+      {
+        this.setState({user})
+      }
+      else{
+        this.setState({user : null})
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.user ? (<Home/>) : (<Login/>)}
+      </div>
+    );
+  }
+}
+
+export default App;
