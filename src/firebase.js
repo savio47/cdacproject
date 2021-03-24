bf582031641d02aa55d86908f45f76cb7956fb17
savio47 <37936071+savio47@users.noreply.github.com>
2021-03-24 15:21:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 //import { db } from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ2y7_9bBKEMZW2gYkg-R5HM6VZY-ElOk",
-  authDomain: "final-project-c1dac.firebaseapp.com",
-  projectId: "final-project-c1dac",
+  apiKey: "add your own key here",
+  authDomain: "",
+  projectId: "",
   storageBucket: "final-project-c1dac.appspot.com",
-  messagingSenderId: "1069810894175",
-  appId: "1:1069810894175:web:d4fcf9a391801e82e322ea"
+  messagingSenderId: "",
+  appId: "1:10682e322ea"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
